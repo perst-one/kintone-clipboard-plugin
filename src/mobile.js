f538f35af0849be0890c8bc9f7a73a36f8414e79
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2020-06-24 01:52:37 +0900

    update デスクトップ版作成

--- src/mobile.js
+++ src/mobile.js
@@ -1,21 +1,5 @@
 jQuery.noConflict();
 
-(function($, PLUGIN_ID) {
-  kintone.events.on('mobile.app.record.index.show', function() {
-    const config = kintone.plugin.app.getConfig(PLUGIN_ID);
+(function(_$, _PLUGIN_ID) {
 
-    const spaceElement = kintone.mobile.app.getHeaderSpaceElement();
-    const fragment = document.createDocumentFragment();
-    const headingEl = document.createElement('h3');
-    const messageEl = document.createElement('p');
-
-    messageEl.classList.add('plugin-space-message');
-    messageEl.textContent = config.message;
-    headingEl.classList.add('plugin-space-heading');
-    headingEl.textContent = 'Hello kintone plugin mobile!';
-
-    fragment.appendChild(headingEl);
-    fragment.appendChild(messageEl);
-    spaceElement.appendChild(fragment);
-  });
 })(jQuery, kintone.$PLUGIN_ID);
